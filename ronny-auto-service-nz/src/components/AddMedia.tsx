265ac3a7f5c9bd56331d473cd46893b6ebea7b9c
vbrnbs <varszegibarnabas@gmail.com>
2023-09-09 13:34:00 +1200

    ui adjusted to the new funcitons

--- ronny-auto-service-nz/src/components/AddMedia.tsx
+++ ronny-auto-service-nz/src/components/AddMedia.tsx
@@ -4,78 +4,98 @@ import WebcamCapture from "./WebcamCapture";
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-
 const firebaseConfig: any = {
-    apiKey: "AIzaSyAyAWLSf9A3e1qQd4N-j_8_CCcSwLKrpts",
-    authDomain: "ronny-nzgorge.firebaseapp.com",
-    projectId: "ronny-nzgorge",
-    storageBucket: "ronny-nzgorge.appspot.com",
-    messagingSenderId: "37839314828",
-    appId: "1:37839314828:web:7b665c39aabf73934d00a9",
-    measurementId: "G-HCV190EENX"
-  };
+  apiKey: "AIzaSyAyAWLSf9A3e1qQd4N-j_8_CCcSwLKrpts",
+  authDomain: "ronny-nzgorge.firebaseapp.com",
+  projectId: "ronny-nzgorge",
+  storageBucket: "ronny-nzgorge.appspot.com",
+  messagingSenderId: "37839314828",
+  appId: "1:37839314828:web:7b665c39aabf73934d00a9",
+  measurementId: "G-HCV190EENX",
+};
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
- 
 function AddMedia() {
-    // State to store uploaded file
-    const [file, setFile] = useState<any>(null);
- 
-    // progress
-    const [percent, setPercent] = useState(0);
- 
-    // Handle file upload event and update state
-    function handleChange(event: any) {
-        handleSetFile(event.target.files[0]);
+  // State to store uploaded file
+  const [file, setFile] = useState<any>(null);
+  const [photo, setPhoto] = useState(false);
+
+  // progress
+  const [percent, setPercent] = useState(0);
+
+  // Handle file upload event and update state
+  function handleChange(event: any) {
+    handleSetFile(event.target.files[0]);
+  }
+
+  function handleSetFile(event: any) {
+    console.log(event);
+    setFile(event);
+  }
+
+  const handleUpload = () => {
+    if (!file) {
+      alert("Please upload an image first!");
     }
 
-    function handleSetFile(event: any) {
-        console.log(event)
-        setFile(event);
-    }
-    
- 
-    const handleUpload = () => {
-        if (!file) {
-            alert("Please upload an image first!");
-        }
- 
-        const storageRef = ref(storage, `/files/${file.name}`);
- 
-        // progress can be paused and resumed. It also exposes progress updates.
-        // Receives the storage reference and the file to upload.
-        const uploadTask = uploadBytesResumable(storageRef, file);
- 
-        uploadTask.on(
-            "state_changed",
-            (snapshot) => {
-                const percent = Math.round(
-                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                );
- 
-                // update progress
-                setPercent(percent);
-            },
-            (err) => console.log(err),
-            () => {
-                // download url
-                getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-                    console.log(url);
-                });
-            }
+    const storageRef = ref(storage, `/files/${file.name}`);
+
+    // progress can be paused and resumed. It also exposes progress updates.
+    // Receives the storage reference and the file to upload.
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const percent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
         );
-    };
- 
-    return (
-        <div>
-            <input type="file" onChange={handleChange} accept="/image/*" />
-            <button onClick={handleUpload}>Upload to Firebase</button>
-            <p>{percent} "% done"</p>
-            <WebcamCapture setFile={setFile} />
-        </div>
+
+        // update progress
+        setPercent(percent);
+      },
+      (err) => console.log(err),
+      () => {
+        // download url
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          console.log(url);
+        });
+      }
     );
+  };
+
+  return (
+    <div className="">
+      {photo ? (
+        <div>
+          <button onClick={() => setPhoto(!photo)}>cancel</button>
+          <WebcamCapture setFile={setFile} />
+          {file && <p>{percent} "% done"</p>}
+        </div>
+      ) : (
+        <div className="">
+          <button onClick={() => setPhoto(!photo)}
+          className="bg-blue-500 p-4">Add photo</button>
+          <input
+            type="file"
+            multiple
+            onChange={handleChange}
+            accept="/image/*"
+          />
+
+          {file && (
+            <div>
+              <button onClick={handleUpload}
+              className="bg-yellow-500 p-4">Upload to Firebase</button>
+              <p>{percent} "% done"</p>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
- 
+
 export default AddMedia;
